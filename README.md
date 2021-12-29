19775d0b72c55da0759624225dae16f48074e7cb
Lee Castelani <notayogilee@gmail.com>
2021-12-29 10:38:57 -0500

    add basic infinite scroll

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Getting Started
 
-npm start
+Clone repo for movieAppAPI
+Follow setup instructions and run on any port 5000
+Clone this repo
+Install dependencies with npm install
+npm run client
--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import {
@@ -9,12 +9,37 @@ import {
 
 const Landing = () => {
   const [movies, setMovies] = useState([]);
+  let [totalPages, setTotalPages] = useState(1);
+  let [page, setPage] = useState(1);
+
+  const movieRequest = useCallback(async (page) => {
+    console.log('REQ MADE')
+    if (page <= totalPages) {
+      const movieResults = await axios.get(`http://localhost:5000/api?page=${page}`);
+      console.log(movieResults)
+      if (page === 1) setTotalPages(movieResults.data.total_pages);
+      setMovies([...movies, ...movieResults.data.results]);
+      const nextPage = page + 1;
+      setPage(nextPage);
+    }
+  },
+    [movies],
+  )
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api')
-      .then((res) => setMovies(res.data.results))
-      .catch((error) => console.log(error))
+    movieRequest(page);
   }, [])
+
+  window.onscroll = function () {
+    let d = document.documentElement;
+    let offset = d.scrollTop + window.innerHeight;
+    let height = d.offsetHeight;
+
+    if (offset + 400 >= height) {
+      console.log('Call next');
+      movieRequest(page);
+    }
+  };
 
   return (
     <Container>
