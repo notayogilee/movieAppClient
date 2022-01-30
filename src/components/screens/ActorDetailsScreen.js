94e1a31dea186f263b4faff838b08d792d13b95c
Lee Castelani <notayogilee@gmail.com>
2022-01-30 16:20:02 -0500

    start layout for actor details page

--- src/components/screens/ActorDetailsScreen.js
+++ src/components/screens/ActorDetailsScreen.js
@@ -3,34 +3,53 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { actorDetails } from '../../actions/actorActions';
 import Navbar from '../utils/Navbar';
+import MovieItem from '../items/MovieItem';
 import {
   Container,
   Typography,
-  Fade
+  Fade,
+  Slide,
+  Grid,
+  Box
 } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
   root: {
-    overflow: 'hidden',
     position: 'relative',
     width: '100%',
     margin: '0',
     padding: '0 !important'
   },
-  image: {
-    pointerEvents: 'none',
+  content: {
     position: 'absolute',
-    width: '100%',
-    height: '100%',
-    opacity: '0.5',
-    zIndex: '-1'
+    display: 'flex',
+    flexDirection: 'column',
+    color: '#f4f4f4',
+    height: '80%',
+    width: '40%',
+    top: '15%',
+    left: '3%'
   },
+  // image: {
+  //   // position: 'absolute',
+  //   width: '35%!important',
+  //   height: '35%',
+
+  // },
+  // details: {
+  //   display: 'flex',
+  //   flexDirection: 'column'
+  // },
+  // bio: {
+
+  // },
   body: {
     height: '100vh',
     width: '100vw',
     margin: '0'
-  }
+  },
+
 });
 
 const ActorDetails = () => {
@@ -55,8 +74,10 @@ const ActorDetails = () => {
       movieCredits,
       showCredits
     }
-
   } = stateActorDetails;
+
+  const { profiles } = actorImages;
+  console.log(profiles)
 
   const location = useLocation();
   const { actor } = location.state;
@@ -72,14 +93,46 @@ const ActorDetails = () => {
     <Container maxWidth={false} className={classes.root}>
       <Navbar />
 
-      <Fade timeout={750} in={!loading}>
+      <Fade in={!loading}>
         <div className={classes.body}>
-          <img className={classes.image} src={`https://www.themoviedb.org/t/p/original${profile_path}`} />
-          <Typography variant="h1">
-            {name}
-          </Typography>
+
+          <Slide timeout={750} direction='right' in={!loading}>
+
+            <div className={classes.content}>
+              <div style={{ display: 'flex', }}>
+                <img height="450" width="300" src={`https://www.themoviedb.org/t/p/original${profile_path}`} />
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', width: '100%' }}>
+                  {profiles && profiles.map((extraImage) => (
+                    <img key={extraImage.file_path} height="150" width="100" src={`https://www.themoviedb.org/t/p/original${extraImage.file_path}`} />
+                  ))
+                  }
+                </div>
+              </div>
+
+              <div>
+                <Typography style={{ display: 'inline-block' }} variant="h4">
+                  {name}
+                </Typography>
+                <Typography variant="h5">
+                  Born: {birthday}
+                </Typography>
+                {deathday &&
+                  <Typography variant="h5">
+                    Died: {deathday}
+                  </Typography>
+                }
+                <Typography variant="h6">
+                  {biography}
+                </Typography>
+              </div>
+            </div>
+          </Slide>
+
+
+
         </div>
       </Fade>
+
     </Container>
   )
 }
